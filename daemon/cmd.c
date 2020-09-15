e8e66634878db0441fb59327883dd8f9f10a6936
Bodong Wang <bodong@mellanox.com>
2020-09-14 20:43:22 -0500

    Daemon: Add cmd to del session
    Signed-off-by: Bodong Wang <bodong@mellanox.com>

--- daemon/cmd.c
+++ daemon/cmd.c
@@ -70,7 +70,10 @@ cmd_flow_query_parsed(void *parsed_result,
 	struct cmd_flow_query_result *res = parsed_result;
 	sessionResponse_t response;
 
+	memset(&response, 0, sizeof(response));
 	opof_get_session_server(res->session_id, &response);
+	if(response.requestStatus == _REJECTED_SESSION_NONEXISTENT)
+		printf("No such session (%d)\n", res->session_id);
 }
 
 cmdline_parse_token_string_t cmd_flow_query_tok =
@@ -83,7 +86,7 @@ cmdline_parse_token_num_t cmd_session_id =
 cmdline_parse_inst_t cmd_flow_query = {
 	.f = cmd_flow_query_parsed,
 	.data = NULL,
-	.help_str = "Query flow <flow-id>",
+	.help_str = "Query flow <session-id>",
 	.tokens = {
 		(void *)&cmd_flow_query_tok,
 		(void *)&cmd_session_id,
@@ -91,9 +94,46 @@ cmdline_parse_inst_t cmd_flow_query = {
 	},
 };
 
+/*** flow_del***/
+
+struct cmd_flow_del_result {
+	cmdline_fixed_string_t flow_del;
+	uint64_t session_id;
+};
+
+static void
+cmd_flow_del_parsed(void *parsed_result,
+		__rte_unused struct cmdline *cl,
+		__rte_unused void *data)
+{
+	struct cmd_flow_del_result *res = parsed_result;
+	sessionResponse_t response;
+
+	memset(&response, 0, sizeof(response));
+	opof_del_session_server(res->session_id, &response);
+	if(response.requestStatus == _REJECTED_SESSION_NONEXISTENT)
+		printf("No such session (%d)\n", res->session_id);
+}
+
+cmdline_parse_token_string_t cmd_flow_del_tok =
+	TOKEN_STRING_INITIALIZER(struct cmd_flow_del_result, flow_del,
+				 "flow del");
+
+cmdline_parse_inst_t cmd_flow_del = {
+	.f = cmd_flow_del_parsed,
+	.data = NULL,
+	.help_str = "Del flow <session-id>",
+	.tokens = {
+		(void *)&cmd_flow_del_tok,
+		(void *)&cmd_session_id,
+		NULL,
+	},
+};
+
 cmdline_parse_ctx_t main_ctx[] = {
-		(cmdline_parse_inst_t *)&cmd_quit,
-		(cmdline_parse_inst_t *)&cmd_flow_query,
+	(cmdline_parse_inst_t *)&cmd_quit,
+	(cmdline_parse_inst_t *)&cmd_flow_query,
+	(cmdline_parse_inst_t *)&cmd_flow_del,
 	NULL,
 };
 
