kylepstein/firewall_offload

commit 134dac04f6386eb60750bb88f24ead2afd4855e3

Daemon: One session manages two flows
IN_PORT will be the one to insert offload flows for both IN and OUT
ports. Aging will be only done when the flow_in is aging out, and
both flow_in and flow_out will be removed at the same time.

Flow query now return the out_packets which queries flow_out.

Signed-off-by: Bodong Wang <bodong@mellanox.com>

daemon/flow.c
@@ -146,7 +146,8 @@ add_simple_flow(uint16_t port_id,
 
 int offload_flow_add(portid_t port_id,
 		     struct fw_session *session,
-		     enum flow_action action)
+		     enum flow_action action,
+		     enum flow_dir dir)
 {
 	struct eth_ntuple_filter *ntuple_filter;
 	struct rte_flow_item_ipv4 ipv4_spec;
@@ -175,8 +176,13 @@ int offload_flow_add(portid_t port_id,
 	/* set up parameters for validate and add */
 	memset(&ipv4_spec, 0, sizeof(ipv4_spec));
 	ipv4_spec.hdr.next_proto_id = ntuple_filter->proto;
-	ipv4_spec.hdr.src_addr = htonl(ntuple_filter->src_ip);
-	ipv4_spec.hdr.dst_addr = htonl(ntuple_filter->dst_ip);
+	if (dir == DIR_IN) {
+		ipv4_spec.hdr.src_addr = htonl(ntuple_filter->src_ip);
+		ipv4_spec.hdr.dst_addr = htonl(ntuple_filter->dst_ip);
+	} else if (dir == DIR_OUT) {
+		ipv4_spec.hdr.src_addr = htonl(ntuple_filter->dst_ip);
+		ipv4_spec.hdr.dst_addr = htonl(ntuple_filter->src_ip);
+	}
 	ipv4_proto = ipv4_spec.hdr.next_proto_id;
 
 	memset(&ipv4_mask, 0, sizeof(ipv4_mask));
@@ -193,8 +199,13 @@ int offload_flow_add(portid_t port_id,
 		ipv4_udp_item.mask = &ipv4_mask;
 		ipv4_udp_item.last = NULL;
 
-		udp_spec.hdr.src_port = htons(ntuple_filter->src_port);
-		udp_spec.hdr.dst_port = htons(ntuple_filter->dst_port);
+		if (dir == DIR_IN) {
+			udp_spec.hdr.src_port = htons(ntuple_filter->src_port);
+			udp_spec.hdr.dst_port = htons(ntuple_filter->dst_port);
+		} else if (dir == DIR_OUT) {
+			udp_spec.hdr.src_port = htons(ntuple_filter->dst_port);
+			udp_spec.hdr.dst_port = htons(ntuple_filter->src_port);
+		}
 		udp_spec.hdr.dgram_len = 0;
 		udp_spec.hdr.dgram_cksum = 0;
 
@@ -219,8 +230,15 @@ int offload_flow_add(portid_t port_id,
 		ipv4_tcp_item.last = NULL;
 
 		memset(&tcp_spec, 0, sizeof(tcp_spec));
-		tcp_spec.hdr.src_port = htons(ntuple_filter->src_port);
-		tcp_spec.hdr.dst_port = htons(ntuple_filter->dst_port);
+
+		if (dir == DIR_IN) {
+			tcp_spec.hdr.src_port = htons(ntuple_filter->src_port);
+			tcp_spec.hdr.dst_port = htons(ntuple_filter->dst_port);
+		} else if (dir == DIR_OUT) {
+			tcp_spec.hdr.src_port = htons(ntuple_filter->dst_port);
+			tcp_spec.hdr.dst_port = htons(ntuple_filter->src_port);
+		}
+
 		tcp_spec.hdr.tcp_flags = 0;
 
 		memset(&tcp_mask, 0, sizeof(tcp_mask));
@@ -249,7 +267,11 @@ int offload_flow_add(portid_t port_id,
 	pattern_ipv4_5tuple[0] = eth_item;
 	pattern_ipv4_5tuple[3] = end_item;
 
-	age.context = session;
+	/* Aging is only done based on DIR_IN */
+	if (dir == DIR_IN)
+		age.context = session;
+	else if (dir == DIR_OUT)
+		age.context = NULL;
 
 	switch(action)
 	{
@@ -275,7 +297,11 @@ int offload_flow_add(portid_t port_id,
 	flow = add_simple_flow(port_id, &attr, pattern_ipv4_5tuple,
 			       actions, "offload");
 
-	session->flow_in = flow;
+	if (dir == DIR_IN)
+		session->flow_in = flow;
+
+	if (dir == DIR_OUT)
+		session->flow_out = flow;
 
 	return 0;
 }

daemon/common.h
@@ -97,6 +97,11 @@ enum flow_action {
 	ACTION_FORWARD	= 1
 };
 
+enum flow_dir {
+	DIR_IN	= 0,
+	DIR_OUT	= 1
+};
+
 struct lcore_priv {
 	enum lcore_type	type;
 	uint8_t		id;
@@ -210,7 +215,8 @@ add_simple_flow(uint16_t port_id,
 
 int offload_flow_add(portid_t port_id,
 		     struct fw_session * session,
-		     enum flow_action action);
+		     enum flow_action action,
+		     enum flow_dir dir);
 int offload_flow_query(portid_t port_id,
 		       struct rte_flow *flow,
 		       uint64_t *packets,

daemon/init.c
@@ -167,6 +167,7 @@ static int create_sample_fwd_flow(uint16_t port_id, int proto,
 	int ret = 0;
 
 	//FIXME: with sample flows, num of flows can't up to 5k
+	return 0;
 
 	memset(&response, 0, sizeof(response));
 	memset(&request, 0, sizeof(request));

daemon/opof_server.c
@@ -49,7 +49,15 @@ int opof_del_flow(struct fw_session *session)
 	struct rte_hash *ht = off_config_g.session_ht;
 	int ret;
 
-	ret = offload_flow_destroy(session->port_in, session->flow_in);
+	ret = offload_flow_destroy(session->port_in,session->flow_in);
+
+	if (ret) {
+		ret = FAILURE;
+		goto out;
+	}
+
+	ret = offload_flow_destroy(session->port_out,
+				   session->flow_out);
 
 	if (ret) {
 		ret = FAILURE;
@@ -113,18 +121,33 @@ int opof_add_session_server(sessionRequest_t *parameters,
 	}
 
 	ret = offload_flow_add(session->port_in, session,
-			       (enum flow_action)parameters->actType);
+			       (enum flow_action)parameters->actType,
+			       DIR_IN);
+
+	if (ret)
+		goto out;
+
+	ret = offload_flow_add(session->port_out, session,
+			       (enum flow_action)parameters->actType,
+			       DIR_OUT);
 
 	if (!ret) {
 		session->state = _ESTABLISHED;
 		rte_hash_add_key_data(ht, &session->key, (void *)session);
 		rte_atomic32_inc(&off_config_g.stats.active);
 		offload_dbg("Session (%d) added\n", session->key.sess_id);
+	} else {
+		goto err_flow_out;
 	}
 
-	response->requestStatus = ret ? _REJECTED : _ACCEPTED;
+	response->requestStatus = _ACCEPTED;
 
+	return 0;
+
+err_flow_out:
+	offload_flow_destroy(session->port_in, session->flow_in);
 out:
+	response->requestStatus = _REJECTED;
 	return ret;
 }
 
@@ -149,6 +172,9 @@ int opof_get_session_server(unsigned long sessionId,
 
 	offload_flow_query(session->port_in, session->flow_in,
 			   &response->inPackets, &response->inBytes);
+
+	offload_flow_query(session->port_out, session->flow_out,
+			   &response->outPackets, &response->outBytes);
 
 	response->sessionId = sessionId;
 	response->sessionState = session->state;

commit e25524fd60817412bf899d12ca1eadc2c28d8a74

Deamon: Only do action aging for DIR_IN
Fixes: 134dac04f638 ("Daemon: One session manages two flows")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

daemon/flow.c
@@ -162,7 +162,7 @@ int offload_flow_add(portid_t port_id,
 	struct rte_flow_item tcp_item;
 	struct rte_flow_item pattern_ipv4_5tuple[4];
 	struct rte_flow *flow = NULL;
-	uint8_t ipv4_proto;
+	uint8_t ipv4_proto, i = 0;
 	int ret = -1;
 
 	ntuple_filter = &session->tuple;
@@ -277,17 +277,19 @@ int offload_flow_add(portid_t port_id,
 	{
 	case ACTION_FORWARD:
 		attr.priority = FDB_FWD_PRIORITY;
-		actions[0] = jump_action;
-		actions[1] = age_action;
-		actions[2] = count_action;
-		actions[3] = end_action;
+		actions[i++] = jump_action;
+		if (dir == DIR_IN)
+			actions[i++] = age_action;
+		actions[i++] = count_action;
+		actions[i++] = end_action;
 		break;
 	case ACTION_DROP:
 		attr.priority = FDB_DROP_PRIORITY;
-		actions[0] = drop_action;
-		actions[1] = age_action;
-		actions[2] = count_action;
-		actions[3] = end_action;
+		actions[i++] = drop_action;
+		if (dir == DIR_IN)
+			actions[i++] = age_action;
+		actions[i++] = count_action;
+		actions[i++] = end_action;
 		break;
 	default:
 		printf("Offload flow: invalid action\n");