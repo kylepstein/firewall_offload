kylepstein/firewall_offload

commit 6549eaa2c4a1cb1259cc6360e00b98e28cdc6b2f

Daemon: Only delete session when both directions are aged out
Note that this hides the bug that the counter on on direction is not
updating.

Signed-off-by: Bodong Wang <bodong@mellanox.com>

daemon/common.h
@@ -159,6 +159,8 @@ struct fw_session {
 	uint8_t		state;
 	uint8_t		close_code;
 	uint32_t	timeout;
+
+	rte_atomic32_t	ref_count;
 };
 
 struct offload_stats {

daemon/flow.c
@@ -287,11 +287,7 @@ int offload_flow_add(portid_t port_id,
 		return -EINVAL;
 	}
 
-	/* Aging is only done based on DIR_IN */
-	if (dir == DIR_IN)
-		age.context = session;
-	else if (dir == DIR_OUT)
-		age.context = NULL;
+	age.context = session;
 
 	switch(action)
 	{
@@ -324,6 +320,8 @@ int offload_flow_add(portid_t port_id,
 
 	if (dir == DIR_OUT)
 		session->flow_out = flow;
+
+	rte_atomic32_inc(&session->ref_count);
 
 	return 0;
 }
@@ -366,6 +364,9 @@ int offload_flow_destroy(portid_t port_id, struct rte_flow *flow)
 {
 	struct rte_flow_error error;
 	int ret = 0;
+
+	if (!flow)
+		return 0;
 
 	if (port_id_is_invalid(port_id, ENABLED_WARN) ||
 	    port_id == (portid_t)RTE_PORT_ALL)
@@ -411,10 +412,17 @@ void offload_flow_aged(portid_t port_id)
 		session = (struct fw_session*)contexts[idx];
 		if (!session)
 			continue;
-		session->close_code = _TIMEOUT;
-		ret = opof_del_flow(session);
-		if (!ret)
-			rte_atomic32_inc(&off_config_g.stats.aged);
+
+		/* Only delete flow when both directions are aged out.
+		 * This hides the bug that the counter on one of the
+		 * direction is not updating
+		 */
+		if (rte_atomic32_dec_and_test(&session->ref_count)) {
+			session->close_code = _TIMEOUT;
+			ret = opof_del_flow(session);
+			if (!ret)
+				rte_atomic32_inc(&off_config_g.stats.aged);
+		}
 	}
 	free(contexts);
 }

commit a476462c2626134d501708ffbbd6eb5f8788bed3

Daemon: Workaround for no update on counter of one direction
Each flow in one session has a ref count, it will be set as 1 when
added, set as 0 when aged out. Only when ref counts on both flows
are 0, session is deleted.

Note that dpdk aging seems using age context as the matching criteria
to look up flow, as if two flows has the same context, only one of
them get aging out.

Fixes: 6549eaa2c4a1 ("Daemon: Only delete session when both directions are aged out")
Signed-off-by: Bodong Wang <bodong@mellanox.com>

daemon/common.h
@@ -147,20 +147,23 @@ struct session_key {
 	uint64_t sess_id;
 };
 
+struct offload_flow {
+	struct fw_session *session;
+	struct rte_flow *flow;
+	rte_atomic32_t ref_count;
+	portid_t portid;
+};
+
 struct fw_session {
 	struct session_key		key;
 	struct eth_ntuple_filter	tuple;
 
-	struct rte_flow *flow_in;
-	portid_t	port_in;
-	struct rte_flow *flow_out;
-	portid_t	port_out;
+	struct offload_flow		flow_in;
+	struct offload_flow		flow_out;
 
-	uint8_t		state;
-	uint8_t		close_code;
-	uint32_t	timeout;
-
-	rte_atomic32_t	ref_count;
+	uint8_t				state;
+	uint8_t				close_code;
+	uint32_t			timeout;
 };
 
 struct offload_stats {

daemon/flow.c
@@ -17,15 +17,6 @@ static struct rte_flow_action count_action = {
 	RTE_FLOW_ACTION_TYPE_COUNT, NULL
 };
 
-struct rte_flow_action_age age = {
-	.timeout = DEFAULT_TIMEOUT,
-};
-
-static struct rte_flow_action age_action = {
-	RTE_FLOW_ACTION_TYPE_AGE,
-	&age
-};
-
 struct rte_flow_action_jump nic_rx_group = {
 	.group = NIC_RX_GROUP,
 };
@@ -161,6 +152,13 @@ int offload_flow_add(portid_t port_id,
 	uint8_t flow_index = 0;
 	int ret = -1;
 
+	struct rte_flow_action_age age = {};
+
+	struct rte_flow_action age_action = {
+		RTE_FLOW_ACTION_TYPE_AGE,
+		&age
+	};
+
 	memset(&actions, 0, sizeof(actions));
 	memset(&pattern_ipv4_5tuple, 0, sizeof(pattern_ipv4_5tuple));
 
@@ -287,23 +285,25 @@ int offload_flow_add(portid_t port_id,
 		return -EINVAL;
 	}
 
-	age.context = session;
+	if (dir == DIR_IN) {
+		age.context = &session->flow_in;
+	} else {
+		age.context = &session->flow_out;
+	}
 
 	switch(action)
 	{
 	case ACTION_FORWARD:
 		attr.priority = FDB_FWD_PRIORITY;
 		actions[i++] = jump_action;
-		if (dir == DIR_IN)
-			actions[i++] = age_action;
+		actions[i++] = age_action;
 		actions[i++] = count_action;
 		actions[i++] = end_action;
 		break;
 	case ACTION_DROP:
 		attr.priority = FDB_DROP_PRIORITY;
 		actions[i++] = drop_action;
-		if (dir == DIR_IN)
-			actions[i++] = age_action;
+		actions[i++] = age_action;
 		actions[i++] = count_action;
 		actions[i++] = end_action;
 		break;
@@ -315,13 +315,17 @@ int offload_flow_add(portid_t port_id,
 	flow = add_simple_flow(port_id, &attr, pattern_ipv4_5tuple,
 			       actions, "offload");
 
-	if (dir == DIR_IN)
-		session->flow_in = flow;
-
-	if (dir == DIR_OUT)
-		session->flow_out = flow;
-
-	rte_atomic32_inc(&session->ref_count);
+	if (dir == DIR_IN) {
+		session->flow_in.session = session;
+		session->flow_in.flow = flow;
+		session->flow_in.portid = port_id;
+		rte_atomic32_set(&session->flow_in.ref_count, 1);
+	} else {
+		session->flow_out.session = session;
+		session->flow_out.flow = flow;
+		session->flow_out.portid = port_id;
+		rte_atomic32_set(&session->flow_out.ref_count, 1);
+	}
 
 	return 0;
 }
@@ -384,6 +388,7 @@ void offload_flow_aged(portid_t port_id)
 	int nb_context, total = 0, idx;
 	struct rte_flow_error error;
 	struct fw_session *session;
+	struct offload_flow *flow;
 	void **contexts;
 	int ret;
 
@@ -409,15 +414,19 @@ void offload_flow_aged(portid_t port_id)
 	}
 
 	for (idx = 0; idx < nb_context; idx++) {
-		session = (struct fw_session*)contexts[idx];
-		if (!session)
+		flow = (struct offload_flow *)contexts[idx];
+		if (!flow)
 			continue;
+		session = flow->session;
+
+		rte_atomic32_set(&flow->ref_count, 0);
 
 		/* Only delete flow when both directions are aged out.
 		 * This hides the bug that the counter on one of the
 		 * direction is not updating
 		 */
-		if (rte_atomic32_dec_and_test(&session->ref_count)) {
+		if (!rte_atomic32_read(&session->flow_in.ref_count) &&
+		    !rte_atomic32_read(&session->flow_out.ref_count)) {
 			session->close_code = _TIMEOUT;
 			ret = opof_del_flow(session);
 			if (!ret)

daemon/opof_server.c
@@ -130,10 +130,10 @@ static int __opof_get_session_server(unsigned long sessionId,
 	if (!session)
 		return _NOT_FOUND;
 
-	offload_flow_query(session->port_in, session->flow_in,
+	offload_flow_query(session->flow_in.portid, session->flow_in.flow,
 			   &response->inPackets, &response->inBytes);
 
-	offload_flow_query(session->port_out, session->flow_out,
+	offload_flow_query(session->flow_out.portid, session->flow_out.flow,
 			   &response->outPackets, &response->outBytes);
 
 	response->sessionState = session->state;
@@ -158,13 +158,14 @@ int opof_del_flow(struct fw_session *session)
 	__opof_get_session_server(session->key.sess_id,
 				  session_stat, false);
 
-	ret = offload_flow_destroy(session->port_in,session->flow_in);
+	ret = offload_flow_destroy(session->flow_in.portid,
+				   session->flow_in.flow);
 
 	if (ret)
 		goto out;
 
-	ret = offload_flow_destroy(session->port_out,
-				   session->flow_out);
+	ret = offload_flow_destroy(session->flow_out.portid,
+				   session->flow_out.flow);
 
 	if (ret)
 		goto out;
@@ -230,21 +231,21 @@ int opof_add_session_server(sessionRequest_t *parameters,
 	}
 
 	if ((parameters->inlif & 0xFFFF) == 1) {
-		session->port_in = INITIATOR_PORT_ID;
-		session->port_out = RESPONDER_PORT_ID;
+		session->flow_in.portid = INITIATOR_PORT_ID;
+		session->flow_out.portid = RESPONDER_PORT_ID;
 	} else {
-		session->port_in = RESPONDER_PORT_ID;
-		session->port_out = INITIATOR_PORT_ID;
+		session->flow_in.portid = RESPONDER_PORT_ID;
+		session->flow_out.portid = INITIATOR_PORT_ID;
 	}
 
-	ret = offload_flow_add(session->port_in, session,
+	ret = offload_flow_add(session->flow_in.portid, session,
 			       (enum flow_action)parameters->actType,
 			       DIR_IN);
 
 	if (ret)
 		return _INTERNAL;
 
-	ret = offload_flow_add(session->port_out, session,
+	ret = offload_flow_add(session->flow_out.portid, session,
 			       (enum flow_action)parameters->actType,
 			       DIR_OUT);
 
@@ -253,7 +254,8 @@ int opof_add_session_server(sessionRequest_t *parameters,
 		rte_hash_add_key_data(ht, &session->key, (void *)session);
 		rte_atomic32_inc(&off_config_g.stats.active);
 	} else {
-		offload_flow_destroy(session->port_in, session->flow_in);
+		offload_flow_destroy(session->flow_in.portid,
+				     session->flow_in.flow);
 		printf("ERR(%d): Failed to add session (%lu)\n",
 		       ret, session->key.sess_id);
 		return _INTERNAL;